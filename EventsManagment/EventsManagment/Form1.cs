69ed3a97cea4d38cf9fa4ea8281b91119f127a15
Daniel Rota <72995278+DanielRota@users.noreply.github.com>
2022-04-11 20:08:36 +0200

    Add files via upload

---
+++ EventsManagment/EventsManagment/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Forms;
+
+namespace EventsManagment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+        }
+
+        // Refers to the new EventHandler: ChangeNameEventArgs, not EventArgs anymore
+        private static void AlertChange(object sender, ChangeNameEventArgs e)
+        {
+            MessageBox.Show("Event unleashed!\n" + $"OldName: {e.OldName} - NewName: {e.NewName}");
+        }
+
+        // When Name gets changed
+        private void ChangeName(object sender, System.EventArgs e)
+        {
+            // Instance Example Class
+            var example = new Example("Daniel");
+
+            // Assign things to ChangeName Event
+            example.ChangeName += AlertChange;
+
+            // Unleash associated Event (AlertChange) and 
+            example.Name = RTXTName.Text;
+        }
+    }
+}
